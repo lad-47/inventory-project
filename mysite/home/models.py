eb8d0e2e569f844c49f653c1c0cdea55838240ae
Lucas Donaldson <l.donaldson1995@yahoo.com>
2017-01-27 15:36:36 -0500

    updated database with Item, Tag, and Request tables

--- mysite/home/models.py
+++ mysite/home/models.py
@@ -1,8 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Item(models.Model):
-	item_name = models.CharField(max_length=200)
-	count = models.IntegerField(default=0)
+	item_name = models.CharField(max_length=100)
+	total_count = models.IntegerField(default=0)
+	total_available = models.IntegerField(default=0)
+	model_number = models.CharField(max_length=100, null=True)
+	description = models.TextField(null=True)
+	location = models.CharField(max_length=100,null=True)
 	def __str__(self):
 		return self.item_name
+		
+class Tag(models.Model):
+	item_id = models.ForeignKey(Item, on_delete=models.CASCADE)
+	tag = models.CharField(max_length=100)
+	
+class Request(models.Model):
+	STATUSES = (
+	('O','Outstanding'),
+	('A','Approved'),
+	('D','Denied'))
+	user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+	item_id = models.ForeignKey(Item, on_delete=models.CASCADE)
+	reason = models.TextField()
+	status = models.CharField(max_length=1, choices=STATUSES)
+	
+	
---
+++ mysite/inventory_db/load.sql
@@ -0,0 +1,2 @@
+\COPY home_item(id,item_name,total_count,total_available,model_number,description,location) FROM 'sample_data/Item.dat' WITH DELIMITER ',' NULL '' CSV
+\COPY home_tag(id,item_id_id,tag) FROM 'sample_data/Tag.dat' WITH DELIMITER ',' NULL '' CSV
