bb650a6216ce0678511c6ad3bbdf320526e9348b
Lucas Donaldson <l.donaldson1995@yahoo.com>
2017-02-26 12:04:12 -0500

    added custom fields to rest api

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -31,6 +31,17 @@ urlpatterns = [
     url(r'^api/user/(?P<pk>[0-9]+)$', api_views.user_detail, name='user-detail'),
     url(r'^api/user/create$', api_views.user_create, name='user-create'),
     url(r'^api/$', api_views.api_root, name='api'),
+    url(r'^api/custom_fields/$', api_views.custom_root, name='api-custom'),
+    url(r'^api/short$', api_views.short_list, name='short-list'),
+    url(r'^api/short/(?P<pk>[0-9]+)$', api_views.short_detail, name='short-detail'),
+    url(r'^api/long$', api_views.long_list, name='long-list'),
+    url(r'^api/long/(?P<pk>[0-9]+)$', api_views.long_detail, name='long-detail'),
+    url(r'^api/int$', api_views.int_list, name='int-list'),
+    url(r'^api/int/(?P<pk>[0-9]+)$', api_views.int_detail, name='int-detail'),
+    url(r'^api/float$', api_views.float_list, name='float-list'),
+    url(r'^api/float/(?P<pk>[0-9]+)$', api_views.float_detail, name='float-detail'),
+    url(r'^api/entry$', api_views.custom_list, name='custom-list'),
+    url(r'^api/entry/(?P<pk>[0-9]+)$', api_views.custom_detail, name='custom-detail'),
     url(r'^token/$', api_views.get_token, name='get-token'),
 ]
 
